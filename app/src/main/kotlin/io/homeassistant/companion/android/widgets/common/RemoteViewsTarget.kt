96a7309b7f1aafd302a6c9e0022a6c3ab1ed7b13
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-03-26 08:51:18 +0100

    Fix race condition with coil RemoteViewsTarget (#6627)

--- app/src/main/kotlin/io/homeassistant/companion/android/widgets/common/RemoteViewsTarget.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/widgets/common/RemoteViewsTarget.kt
@@ -1,7 +1,5 @@
 package io.homeassistant.companion.android.widgets.common
 
-import android.appwidget.AppWidgetManager
-import android.content.Context
 import android.widget.RemoteViews
 import androidx.annotation.IdRes
 import coil3.Image
@@ -9,15 +7,16 @@ import coil3.target.Target
 import coil3.toBitmap
 
 /**
- * Load images into RemoteViews with Coil
- * (based on https://coil-kt.github.io/coil/recipes/#remote-views)
+ * [Target] implementation to load images into a [RemoteViews] ImageView with Coil.
+ *
+ * This target only mutates the provided [remoteViews] instance by setting the bitmap on
+ * [imageViewResId]. It does **not** call [android.appwidget.AppWidgetManager.updateAppWidget].
+ * Callers are responsible for triggering the widget update themselves after the request
+ * completes (for example, in the surrounding widget update logic).
+ *
+ * Based on https://coil-kt.github.io/coil/recipes/#remote-views
  */
-class RemoteViewsTarget(
-    private val context: Context,
-    private val appWidgetId: Int,
-    private val remoteViews: RemoteViews,
-    @IdRes private val imageViewResId: Int,
-) : Target {
+class RemoteViewsTarget(private val remoteViews: RemoteViews, @IdRes private val imageViewResId: Int) : Target {
 
     override fun onStart(placeholder: Image?) {
         // Skip if null to avoid blinking (there is no placeholder)
@@ -30,6 +29,5 @@ class RemoteViewsTarget(
 
     private fun setDrawable(image: Image?) {
         remoteViews.setImageViewBitmap(imageViewResId, image?.toBitmap())
-        AppWidgetManager.getInstance(context).updateAppWidget(appWidgetId, remoteViews)
     }
 }
